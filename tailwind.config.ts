0bcb797d607a64090aff346bace197de534ef36a
Philip <philipchen935@gmail.com>
2024-10-26 23:06:24 -0400

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        darkWhite: "#f9f8f6",
+        grey: "#e8e6e3",
+        greyText: "#777169"
       },
     },
   },
